be6718b57dae1795ef1e74c51ed65f725c984960
kanekotic <alvarojosepl@gmail.com>
2018-11-30 10:41:53 +0100

    removed unused library

--- src/stream-binder.js
+++ src/stream-binder.js
@@ -1,7 +1,6 @@
 const constants = require('./constants'),
     dgram = require('dgram'),
-    server = dgram.createSocket('udp4'),
-    udp = require('datagram-stream')
+    server = dgram.createSocket('udp4')
 
 const videoStreamStart = () => new Promise((resolve, reject) => server.bind(constants.ports.video, constants.hosts.local, (error, result) => {
     if(error)
