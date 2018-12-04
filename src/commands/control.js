d32276a0173fd52bf1721ac7fa8c4b4b1075ff4d
kanekotic <alvarojosepl@gmail.com>
2018-12-04 09:50:34 +0100

    added more control commands

--- src/commands/control.js
+++ src/commands/control.js
@@ -2,10 +2,44 @@ const commander = require('../exchanger')
 
 const connect = () => commander.send('command')
 
-const takeoff = () => commander.send('takeoff')
+const takeOff = () => commander.send('takeoff')
 
 const land = () => commander.send('land')
 
+const emergency = () => commander.send('emergency')
+
+const stop = () => commander.send('stop')
+
 const flip = (side) => commander.send(`flip ${side}`)
 
-module.exports = { connect, takeoff, land, flip }
+const up = (distance) => commander.send(`up ${distance}`)
+
+const down = (distance) => commander.send(`down ${distance}`)
+
+const left = (distance) => commander.send(`left ${distance}`)
+
+const right = (distance) => commander.send(`right ${distance}`)
+
+const front = (distance) => commander.send(`forward ${distance}`)
+
+const back = (distance) => commander.send(`back ${distance}`)
+
+const clockwise = (angle) => commander.send(`cw ${angle}`)
+
+const counterClockwise = (angle) => commander.send(`ccw ${angle}`)
+
+module.exports = { 
+    connect, 
+    takeOff, 
+    land, 
+    emergency,
+    stop,
+    move: { up, down, left, right, back, front },
+    rotate: { clockwise, counterClockwise },
+    flip: {
+        left: () => flip('l'),
+        right: () => flip('r'),
+        back: () => flip('b'),
+        front: () => flip('f'),
+    } 
+}
--- test/commands/control-test.js
+++ test/commands/control-test.js
@@ -13,13 +13,91 @@ describe('control commands', () => {
     })
 
     it('should have command for takeoff', () => {
-        controlCommands.takeoff()
+        controlCommands.takeOff()
         expect(commander.send).toBeCalledWith('takeoff')
     })
 
     it('should have command for land', () => {
         controlCommands.land()
         expect(commander.send).toBeCalledWith('land')
+    })
+
+    it('should have command for emergency', () => {
+        controlCommands.emergency()
+        expect(commander.send).toBeCalledWith('emergency')
+    })
+
+    it('should have command for stop', () => {
+        controlCommands.stop()
+        expect(commander.send).toBeCalledWith('stop')
+    })
+
+    it('should have command for flip left', () => {
+        controlCommands.flip.left()
+        expect(commander.send).toBeCalledWith('flip l')
+    })
+
+    it('should have command for flip right', () => {
+        controlCommands.flip.right()
+        expect(commander.send).toBeCalledWith('flip r')
+    })
+
+    it('should have command for flip front', () => {
+        controlCommands.flip.front()
+        expect(commander.send).toBeCalledWith('flip f')
+    })
+
+    it('should have command for flip back', () => {
+        controlCommands.flip.back()
+        expect(commander.send).toBeCalledWith('flip b')
+    })
+
+    it('should have command for go up', () => {
+        const distance = faker.random.number(500)
+        controlCommands.move.up(distance)
+        expect(commander.send).toBeCalledWith(`up ${distance}`)
+    })
+
+    it('should have command for go down', () => {
+        const distance = faker.random.number(500)
+        controlCommands.move.down(distance)
+        expect(commander.send).toBeCalledWith(`down ${distance}`)
+    })
+
+    it('should have command for go left', () => {
+        const distance = faker.random.number(500)
+        controlCommands.move.left(distance)
+        expect(commander.send).toBeCalledWith(`left ${distance}`)
+    })
+
+    it('should have command for go front', () => {
+        const distance = faker.random.number(500)
+        controlCommands.move.front(distance)
+        expect(commander.send).toBeCalledWith(`forward ${distance}`)
+    })
+
+    it('should have command for go back', () => {
+        const distance = faker.random.number(500)
+        controlCommands.move.back(distance)
+        expect(commander.send).toBeCalledWith(`back ${distance}`)
+    })
+
+    it('should have command for go right', () => {
+        const distance = faker.random.number(500)
+        controlCommands.move.right(distance)
+        expect(commander.send).toBeCalledWith(`right ${distance}`)
+    })
+
+    it('should have command for rotate clockwise', () => {
+        const angle = faker.random.number(360)
+        controlCommands.rotate.clockwise(angle)
+        expect(commander.send).toBeCalledWith(`cw ${angle}`)
+    })
+
+    it('should have command for rotate clockwise', () => {
+        const angle = faker.random.number(360)
+        controlCommands.rotate.counterClockwise(angle)
+        expect(commander.send).toBeCalledWith(`ccw ${angle}`)
     })
 
 })
