339b0452575856e66f9a590ca9396eba385527c1
kanekotic <alvarojosepl@gmail.com>
2018-12-18 15:11:15 -0600

    refactor code to split big method

--- src/exchanger.js
+++ src/exchanger.js
@@ -11,30 +11,40 @@ client.on('message', (msg,info) => {
 
 client.bind(constants.ports.response)
 
+const bindStateManagement = (resolve, reject) => {
+    let timeoutId = setTimeout(() => {
+        _local.state = "error"
+    }, 10000);
+    let intervalId = setInterval(() => {
+        if(isIdle())
+            return
+        if(isError())
+            reject(_local.state)
+        else
+            resolve(_local.state)
+        clearInterval(intervalId)
+        clearTimeout(timeoutId)
+        _local.state = "idle"
+    }, 100);
+}
+
+const isIdle = () => _local.state === "idle"
+const isError = () => _local.state === "error"
+
+const transmit = (command) => {
+    const message = Buffer.from(command)
+    client.send(message, 0, message.length, constants.ports.command, constants.hosts.remote, (error) => {
+        if(error)
+            _local.state = "error"
+    })
+}
+
 const send = (command) => {
     return new Promise((resolve, reject) => {
-        if(_local.state !== "idle")
+        if(!isIdle())
             reject("error")
-        const message = Buffer.from(command)
-        client.send(message, 0, message.length, constants.ports.command, constants.hosts.remote, (error) => {
-            if(error)
-                _local.state = "error"
-        })
-        let timeoutId = setTimeout(() => {
-            _local.state = "error"
-        }, 10000);
-        let intervalId = setInterval(() => {
-            if(_local.state === "idle")
-                return
-            if(_local.state === "error")
-                reject(_local.state)
-            else
-                resolve(_local.state)
-            clearInterval(intervalId)
-            clearTimeout(timeoutId)
-            _local.state = "idle"
-        }, 100);
-        
+        bindStateManagement(resolve,reject)        
+        transmit(command)
     })
 }
 
