aeaa8072c34494fe150b20199f353f5e002546e1
kanekotic <alvarojosepl@gmail.com>
2018-12-19 09:38:32 -0600

    added mapping of status

--- src/streams/state.js
+++ src/streams/state.js
@@ -5,8 +5,34 @@ const dgram = require('dgram'),
     _local = {
         emitter: undefined
     }
+    
+const format = (mapped) => ({
+    pitch: mapped.pitch, 
+    roll: mapped.roll, 
+    yaw: mapped.yaw,
+    speed: { x: mapped.vgx, y: mapped.vgy, z: mapped.vgz },
+    temperature: { low: mapped.templ, high: mapped.temph },
+    tof: mapped.tof,
+    heigh: mapped.h,
+    battery: mapped.bat,
+    barometer: mapped.baro,
+    time: mapped.time,
+    acceleration: { x: mapped.agx, y: mapped.agy, z: mapped.agz}
+})
 
-client.on('message', message => _local.emitter.emit('message', message))
+const map = (message) => {
+    let mapped = message.toString()
+        .slice(0, -1)
+        .split(';')
+        .map(element => element.split(':'))
+        .reduce((acc, [key, value]) => {
+            acc[key] = Number(value)
+            return acc
+        }, {})
+
+    return format(mapped)
+}    
+client.on('message', message => _local.emitter.emit('message', map(message)))
 
 const bind = () => {
     client.bind(constants.ports.state)
--- test/streams/state-test.js
+++ test/streams/state-test.js
@@ -37,17 +37,66 @@ describe('state stream', () => {
         expect(mockSocket.bind).toBeCalledWith(constants.ports.state)
     })
 
+const messageGenerator = () => {
+    const pitch = faker.random.number(100),
+        roll = faker.random.number(100),
+        yaw = faker.random.number(100),
+        vgx = faker.random.number(100),
+        vgy = faker.random.number(100),
+        vgz = faker.random.number(100),
+        templ = faker.random.number(100),
+        temph = faker.random.number(100),
+        tof = faker.random.number(100),
+        h = faker.random.number(100),
+        bat = faker.random.number(100),
+        baro = faker.random.number(100),
+        time = faker.random.number(100),
+        agx = faker.random.number(100),
+        agy = faker.random.number(100),
+        agz = faker.random.number(100),
+        resultString = `pitch:${pitch};roll:${roll};yaw:${yaw};vgx:${vgx};vgy:${vgy};vgz:${vgz};templ:${templ};temph:${temph};tof:${tof};h:${h};bat:${bat};baro:${baro};time:${time};agx:${agx};agy:${agy};agz:${agz};`
+}
+//pitch:0;roll:0;yaw:0;vgx:0;vgy:0;vgz:0;templ:80;temph:82;tof:10;h:0;bat:88;baro:23.88;time:0;agx:-8.00;agy:-12.00;agz:-990.00;
     it('should get an event Emitter that receives messages from udp', async () => {
         const emitter = state.bind(),
-            message = faker.random.uuid(),
+            pitch = faker.random.number(100),
+            roll = faker.random.number(100),
+            yaw = faker.random.number(100),
+            vgx = faker.random.number(100),
+            vgy = faker.random.number(100),
+            vgz = faker.random.number(100),
+            templ = faker.random.number(100),
+            temph = faker.random.number(100),
+            tof = faker.random.number(100),
+            h = faker.random.number(100),
+            bat = faker.random.number(100),
+            baro = faker.random.number(100),
+            time = faker.random.number(100),
+            agx = faker.random.number(100),
+            agy = faker.random.number(100),
+            agz = faker.random.number(100),
+            message = `pitch:${pitch};roll:${roll};yaw:${yaw};vgx:${vgx};vgy:${vgy};vgz:${vgz};templ:${templ};temph:${temph};tof:${tof};h:${h};bat:${bat};baro:${baro};time:${time};agx:${agx};agy:${agy};agz:${agz};`,
+            expected = {
+                pitch, 
+                roll, 
+                yaw,
+                speed: { x: vgx, y:vgy, z:vgz },
+                temperature: { low: templ, high: temph },
+                tof,
+                heigh: h,
+                battery: bat,
+                barometer: baro,
+                time,
+                acceleration: { x: agx, y: agy, z: agz}
+            },
             result = new Promise((resolve, reject) => {
                 emitter.on('message', (res) => {
                     resolve(res)
                   });
             })
-        mockSocket.emit('message', message)
+        mockSocket.emit('message', Buffer.from(message, 'utf8'))
         expect(emitter).toBeInstanceOf(EventEmitter)
-        expect(await result).toEqual(message)
+        expect(await result).toEqual(expected)
     })
 
     it('should close socket on listening port and reset emitter', () => {
